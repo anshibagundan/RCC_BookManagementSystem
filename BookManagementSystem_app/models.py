e2419c1fb3fb7e1e9b610f8b965a54e34f40697f
tarakokko3233 <tanahashist1230@outlook.jp>
2024-03-28 17:43:35 +0900

    モデル変更

--- BookManagementSystem_app/models.py
+++ BookManagementSystem_app/models.py
@@ -3,10 +3,16 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    user = models.CharField(max_length=255, blank=True, null=True)
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True)
     isborrow = models.BooleanField(default=False)
     num1 = models.IntegerField(default=0)
     num2 = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
