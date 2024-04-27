becc6d19b73ff31e9ffe2e0cb124b15f10bf1235
tarakokko3233 <tanahashist1230@outlook.jp>
2024-04-27 11:46:39 +0900

    localhost

--- BookManagementSystem/settings.py
+++ BookManagementSystem/settings.py
@@ -34,7 +34,8 @@ SECRET_KEY = 'django-insecure-w1uno(p83_95-!3aekht%p%zw#pm*d@7-wkkuo4i)x18p2&_xx
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
-                 '.herokuapp.com',]
+                 '.herokuapp.com',
+                 'localhost']
 
 
 # Application definition
